2a22977c6ff5b608b7516406c8c79e811b2354f9
pcamarajr <pcamarajr@gmail.com>
2024-07-13 14:28:24 +0200

    fix: set checkbox initial checked value

--- app/components/InputCheckbox.tsx
+++ app/components/InputCheckbox.tsx
@@ -3,15 +3,17 @@ import { FC, InputHTMLAttributes } from "react";
 export type InputCheckboxProps = {
   label: string;
   innerRef?: (ref: HTMLInputElement) => void;
-} & Omit<InputHTMLAttributes<HTMLInputElement>, "type" | "ref">;
+  value?: boolean;
+} & Omit<InputHTMLAttributes<HTMLInputElement>, "checked" | "type" | "ref">;
 
 export const InputCheckbox: FC<InputCheckboxProps> = ({
   label,
   innerRef,
+  value,
   ...props
 }) => (
   <label>
-    <input ref={innerRef} type="checkbox" {...props} />
+    <input ref={innerRef} type="checkbox" checked={value} {...props} />
     {label}
   </label>
 );
